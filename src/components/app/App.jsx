6829604c49fd20d9d5b013dd78129c30581ab022
hrihoryeu <egoragrig@gmail.com>
2022-04-05 13:31:25 +0400

    lesson 138: react lazy

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,11 @@
-import AppHeader from "../appHeader/AppHeader";
-import { MainPage, ComicsPage, SingleComicPage } from "../pages"
-
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import AppHeader from "../appHeader/AppHeader";
+
+const MainPage = lazy(() => import("../pages/MainPage"))
+const ComicsPage = lazy(() => import("../pages/ComicsPage"))
+const SingleComicPage = lazy(() => import("../pages/SingleComicPage"))
 
 const App = () => {
     return (
@@ -9,11 +13,13 @@ const App = () => {
             <div className='app'>
                 <AppHeader />
                 <main>
-                    <Routes>
-                        <Route path='/' element={<MainPage />} />
-                        <Route path='/comics' element={<ComicsPage />} />
-                        <Route path='/comics/:comicId' element={<SingleComicPage />} />
-                    </Routes>
+                    <Suspense fallback={<span>loading...</span>}>
+                        <Routes>
+                            <Route path='/' element={<MainPage />} />
+                            <Route path='/comics' element={<ComicsPage />} />
+                            <Route path='/comics/:comicId' element={<SingleComicPage />} />
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
